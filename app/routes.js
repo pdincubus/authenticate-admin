3b97ae68918cd741e3d478f37e0339062ef986db
Phil Steer <phil.steer@engineering.digital.dwp.gov.uk>
2019-10-17 09:38:56 +0100

    Add more nav checks, several wording changes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+
+/**
+ * Just to get the nav working nicely
+ */
 router.get('/v1/dashboard-root', function (req, res) {
   res.render('v1/dashboard-root', { 'activeNav': 'dashboard' });
 });
@@ -19,6 +23,26 @@ router.get('/v1/dashboard-organisations', function (req, res) {
 
 router.get('/v1/dashboard-user', function (req, res) {
   res.render('v1/dashboard-user', { 'activeNav': 'users' });
+});
+
+router.get('/v1/operator-list', function (req, res) {
+  res.render('v1/operator-list', { 'activeNav': 'operators' });
+});
+
+router.get('/v1/service-list', function (req, res) {
+  res.render('v1/service-list', { 'activeNav': 'services' });
+});
+
+router.get('/v1/organisation-list', function (req, res) {
+  res.render('v1/organisation-list', { 'activeNav': 'organisations' });
+});
+
+router.get('/v1/user-list', function (req, res) {
+  res.render('v1/user-list', { 'activeNav': 'users' });
+});
+
+router.get('/v1/user-edit', function (req, res) {
+  res.render('v1/user-edit', { 'activeNav': 'users' });
 });
 
 module.exports = router
