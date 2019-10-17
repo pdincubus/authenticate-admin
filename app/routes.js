189871af63566caad47b8b4ff03772c8eec073d5
Phil Steer <phil.steer@engineering.digital.dwp.gov.uk>
2019-10-17 09:24:07 +0100

    Get main nav in place, and active state working

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.get('/v1/dashboard-root', function (req, res) {
+  res.render('v1/dashboard-root', { 'activeNav': 'dashboard' });
+});
+
+router.get('/v1/dashboard-operators', function (req, res) {
+  res.render('v1/dashboard-operators', { 'activeNav': 'operators' });
+});
+
+router.get('/v1/dashboard-services', function (req, res) {
+  res.render('v1/dashboard-services', { 'activeNav': 'services' });
+});
+
+router.get('/v1/dashboard-organisations', function (req, res) {
+  res.render('v1/dashboard-organisations', { 'activeNav': 'organisations' });
+});
+
+router.get('/v1/dashboard-user', function (req, res) {
+  res.render('v1/dashboard-user', { 'activeNav': 'users' });
+});
 
 module.exports = router
