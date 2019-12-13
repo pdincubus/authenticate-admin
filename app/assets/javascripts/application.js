5cdaa55cdced8338148720e156270bff10f9de70
Phil Steer <phil.steer@engineering.digital.dwp.gov.uk>
2019-12-13 09:43:40 +0000

    First pass at MEGASEARCHATRON

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,5 +6,54 @@ if (window.console && window.console.info) {
 }
 
 $(document).ready(function () {
-  window.GOVUKFrontend.initAll()
-})
+  window.GOVUKFrontend.initAll();
+});
+
+var megasearchInputElem = document.getElementById('govuk-megasearch-input');
+var megasearchResultsElemXYZ = document.getElementById('govuk-megasearch-results-xyz');
+var megasearchResultsElemCap = document.getElementById('govuk-megasearch-results-cap');
+var megasearchResultsElemNone = document.getElementById('govuk-megasearch-results-none');
+
+if (megasearchInputElem) {
+    megasearchInputElem.addEventListener('input', function(e) {
+        console.log(this.value);
+
+        if (this.value.toLowerCase() === 'xyz') {
+            megasearchResultsElemXYZ.classList.add('is_active');
+            megasearchResultsElemXYZ.setAttribute('aria-hidden', false);
+
+            megasearchResultsElemCap.classList.remove('is_active');
+            megasearchResultsElemCap.setAttribute('aria-hidden', true);
+
+            megasearchResultsElemNone.classList.remove('is_active');
+            megasearchResultsElemNone.setAttribute('aria-hidden', true);
+        } else if (this.value.toLowerCase() === 'cap') {
+            megasearchResultsElemXYZ.classList.remove('is_active');
+            megasearchResultsElemXYZ.setAttribute('aria-hidden', true);
+
+            megasearchResultsElemCap.classList.add('is_active');
+            megasearchResultsElemCap.setAttribute('aria-hidden', false);
+
+            megasearchResultsElemNone.classList.remove('is_active');
+            megasearchResultsElemNone.setAttribute('aria-hidden', true);
+        } else if (this.value.length >= 3) {
+            megasearchResultsElemXYZ.classList.remove('is_active');
+            megasearchResultsElemXYZ.setAttribute('aria-hidden', true);
+
+            megasearchResultsElemCap.classList.remove('is_active');
+            megasearchResultsElemCap.setAttribute('aria-hidden', true);
+
+            megasearchResultsElemNone.classList.add('is_active');
+            megasearchResultsElemNone.setAttribute('aria-hidden', false);
+        } else {
+            megasearchResultsElemXYZ.classList.remove('is_active');
+            megasearchResultsElemXYZ.setAttribute('aria-hidden', true);
+
+            megasearchResultsElemCap.classList.remove('is_active');
+            megasearchResultsElemCap.setAttribute('aria-hidden', true);
+
+            megasearchResultsElemNone.classList.remove('is_active');
+            megasearchResultsElemNone.setAttribute('aria-hidden', true);
+        }
+    });
+}
