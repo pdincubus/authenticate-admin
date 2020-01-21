0a1c72e2320992f569c7782c6a5886851d6dca32
Phil Steer <phil.steer@engineering.digital.dwp.gov.uk>
2020-01-21 12:26:15 +0000

    Start of V6

--- app/routes.js
+++ app/routes.js
@@ -455,8 +455,6 @@ router.post('/v5/l3/users/edit/delete-branch', function (req, res) {
     }
 });
 
-
-
 /** All level 4 (Standard users) routes */
 router.post('/v5/l4/users/register/app-branch', function (req, res) {
     if (req.session.data['app-backup'] === 'yes') {
@@ -540,6 +538,267 @@ router.post('/v5/index-check', function (req, res) {
             break;
         default:
             res.redirect('/v5/l2/email');
+            break;
+    }
+});
+
+/**
+ * V6
+ * */
+
+router.post('/v6/l2/users/add/org-check', function (req, res) {
+    const chosenOrgName = req.session.data['organisation-name-existing'].toLowerCase();
+    const orgName = req.session.data['organisation-name'];
+
+    if (
+        chosenOrgName == 'capita'
+        || chosenOrgName == 'serco'
+        || chosenOrgName == 'remploy'
+        || chosenOrgName == 'g4s'
+        || chosenOrgName == 'london borough of croydon council'
+        || chosenOrgName == 'sheffield city council'
+        || (chosenOrgName == orgName && orgName !== null)
+        || (chosenOrgName == orgName.toLowerCase() && orgName !== undefined)
+    ) {
+        req.session.data['org-error'] = false;
+
+        res.redirect('/v6/l2/users/add/details');
+    } else {
+        req.session.data['org-error'] = true;
+
+        res.redirect('/v6/l2/users/add/start');
+    }
+});
+
+router.post('/v6/l2/orgs/add/address-branch', function (req, res) {
+    const checkChoice = req.session.data['add-org-address'];
+
+    if (checkChoice === 'yes') {
+        res.redirect('/v6/l2/orgs/add/address');
+    } else {
+        res.redirect('/v6/l2/orgs/add/contacts-check');
+    }
+});
+
+router.post('/v6/l2/orgs/add/contacts-branch', function (req, res) {
+    const checkChoice = req.session.data['add-org-contacts'];
+
+    if (checkChoice === 'yes') {
+        res.redirect('/v6/l2/orgs/add/contact');
+    } else {
+        res.redirect('/v6/l2/orgs/add/summary');
+    }
+});
+
+router.post('/v6/l2/orgs/add/contacts-2-branch', function (req, res) {
+    const checkChoice = req.session.data['add-additional-org-contact'];
+
+    if (checkChoice === 'yes') {
+        res.redirect('/v6/l2/orgs/add/contact-2');
+    } else {
+        res.redirect('/v6/l2/orgs/add/summary');
+    }
+});
+
+router.post('/v6/l2/orgs/add/contacts-3-branch', function (req, res) {
+    const checkChoice = req.session.data['add-another-additional-org-contact'];
+
+    if (checkChoice === 'yes') {
+        res.redirect('/v6/l2/orgs/add/contact-3');
+    } else {
+        res.redirect('/v6/l2/orgs/add/summary');
+    }
+});
+
+router.post('/v6/l2/orgs/add/details-branch', function (req, res) {
+    const orgCompleteDetails = req.session.data['org-complete-details'].toLowerCase();
+    const orgAddress = req.session.data['address-line-1'];
+    const orgContact1 = req.session.data['contact-1-first-name'];
+    const orgContact2 = req.session.data['contact-2-first-name'];
+
+    if (orgCompleteDetails === 'yes') {
+        if (orgContact1 && !orgContact2) {
+            res.redirect('/v6/l2/orgs/add/contact-2');
+        } else if (!orgContact1 && !orgContact2) {
+            res.redirect('/v6/l2/orgs/add/contact');
+        } else if (!orgAddress) {
+            res.redirect('/v6/l2/orgs/add/address-only');
+        }
+    } else if (orgCompleteDetails === 'no') {
+        res.redirect('/v6/l2/orgs/view/custom');
+    }
+});
+
+router.post('/v6/l2/orgs/view/actions-branch', function (req, res) {
+    const orgAction = req.session.data['org-task'];
+
+    console.log(orgAction);
+
+    if (orgAction == 'delete') {
+        res.redirect('/v6/l2/orgs/edit/delete-check');
+    } else if (orgAction == 'delete-users-by-team') {
+        res.redirect('/v6/l2/orgs/edit/delete-by-team-check');
+    } else if (orgAction == 'delete-users-by-service') {
+        res.redirect('/v6/l2/orgs/edit/delete-by-service-check');
+    }
+});
+
+router.post('/v6/l2/orgs/edit/delete', function (req, res) {
+    if (req.session.data['delete-check'] === 'yes') {
+        res.redirect('/v6/l2/orgs/edit/delete-confirm');
+    } else {
+        res.redirect('/v6/l2/orgs/view/all');
+    }
+});
+
+router.post('/v6/l2/orgs/edit/suspend', function (req, res) {
+    if (req.session.data['suspend-check'] === 'yes') {
+        res.redirect('/v6/l2/orgs/edit/suspend-confirm');
+    } else {
+        res.redirect('/v6/l2/orgs/view/all');
+    }
+});
+
+router.post('/v6/l2/users/view/edit-branch', function (req, res) {
+    const editChoice = req.session.data['user-task'];
+
+    if (editChoice === 'reinvite') {
+        res.redirect('/v6/l2/users/edit/reinvite');
+    } else if (editChoice === 'delete') {
+        res.redirect('/v6/l2/users/edit/delete-check');
+    }
+});
+
+router.post('/v6/l2/users/edit/delete-branch', function (req, res) {
+    const deleteChoice = req.session.data['delete-check'];
+
+    if (deleteChoice === 'yes') {
+        res.redirect('/v6/l2/users/edit/delete-confirm');
+    } else {
+        res.redirect('/v6/l2/users/view/single');
+    }
+});
+
+router.post('/v6/l2/users/register/app-branch', function (req, res) {
+    if (req.session.data['app-backup'] === 'yes') {
+        res.redirect('/v6/l2/users/register/qr');
+    } else {
+        res.redirect('/v6/l2/users/register/complete');
+    }
+});
+
+/** All level 3 (Org admin users) routes */
+router.post('/v6/l3/users/register/app-branch', function (req, res) {
+    if (req.session.data['app-backup'] === 'yes') {
+        res.redirect('/v6/l3/users/register/qr');
+    } else {
+        res.redirect('/v6/l3/users/register/complete');
+    }
+});
+
+router.post('/v6/l3/users/view/edit-branch', function (req, res) {
+    const editChoice = req.session.data['user-task'];
+
+    if (editChoice === 'reinvite') {
+        res.redirect('/v6/l3/users/edit/reinvite');
+    } else if (editChoice === 'delete') {
+        res.redirect('/v6/l3/users/edit/delete-check');
+    }
+});
+
+router.post('/v6/l3/users/edit/delete-branch', function (req, res) {
+    const deleteChoice = req.session.data['delete-check'];
+
+    if (deleteChoice === 'yes') {
+        res.redirect('/v6/l3/users/edit/delete-confirm');
+    } else {
+        res.redirect('/v6/l3/users/view/single');
+    }
+});
+
+/** All level 4 (Standard users) routes */
+router.post('/v6/l4/users/register/app-branch', function (req, res) {
+    if (req.session.data['app-backup'] === 'yes') {
+        res.redirect('/v6/l4/users/register/qr');
+    } else {
+        res.redirect('/v6/l4/users/register/complete');
+    }
+});
+
+/** Onboarding, is service contact also an admin */
+router.post('/v6/add-service/admin-branch', function (req, res) {
+    const numContacts = req.session.data['contacts-total'];
+
+    let thereAreAdmins = false;
+
+    console.log(numContacts, 'submitted, check if any are to be admins');
+
+    for (let i = 1; i <= numContacts; i++) {
+        console.log('Checking contact', i, '.', `contact-${i}-admin-check:`, req.session.data[`contact-${i}-admin-check`]);
+
+        const adminCheck = req.session.data[`contact-${i}-admin-check`];
+
+        if (adminCheck === 'yes') {
+            thereAreAdmins = true;
+
+            console.log(i, 'is an admin');
+        } else {
+            console.log(i, 'is not an admin');
+        }
+    }
+
+    console.log('For loop finished');
+
+    if (thereAreAdmins) {
+        console.log('Go to support details');
+
+        res.redirect('/v6/add-service/support');
+    } else {
+        console.log('Go to add service admins');
+
+        res.redirect('/v6/add-service/admins');
+    }
+});
+
+/** Onboarding, add custom data */
+router.post('/v6/add-service/custom-data-branch', function (req, res) {
+    if (req.session.data['custom-data-check'] === 'yes') {
+        res.redirect('/v6/add-service/custom-data-add');
+    } else {
+        res.redirect('/v6/add-service/summary');
+    }
+});
+
+/**
+ * Routes picked up from 'config' on the index view
+ */
+router.post('/v6/index-check', function (req, res) {
+    const routeChosen = req.session.data['prototype-route'];
+
+    switch (routeChosen) {
+        case 'l2-register':
+            res.redirect('/v6/l2/users/register/email');
+            break;
+        case 'l3-register':
+            res.redirect('/v6/l3/users/register/email');
+            break;
+        case 'l4-register':
+            res.redirect('/v6/l4/users/register/email');
+            break;
+        case 'l2-reset':
+            res.redirect('/v6/l2/users/reset-password/start');
+            break;
+        case 'l2-dashboard':
+            res.redirect('/v6/l2/email');
+            break;
+        case 'l3-dashboard':
+            res.redirect('/v6/l3/email');
+            break;
+        case 'l4-dashboard':
+            res.redirect('/v6/l4/email');
+            break;
+        default:
+            res.redirect('/v6/l2/email');
             break;
     }
 });
