90616e14762ae5bc2493b590769b9a8318c6e78d
Stu Bamforth <stuart.bamforth@engineering.digital.dwp.gov.uk>
2020-03-12 14:28:39 +0000

    Updates to sign in pages and header logo text

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'DWP Authenticate',
+  serviceName: 'DWP Authenticate admin portal',
 
   // Default port that prototype runs on
   port: '3000',
